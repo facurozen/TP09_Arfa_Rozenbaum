69eaeaeed2a20c32358a6860cab6d217fe18d77b
FACUNDO ROZENBAUM <46871290@ort>
2022-10-06 12:08:16 -0300

    Login

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP09_Arfa_Rozenbaum.Models;
+
+namespace TP09_Arfa_Rozenbaum.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View("Login");
+    }
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP09_Arfa_Rozenbaum.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
